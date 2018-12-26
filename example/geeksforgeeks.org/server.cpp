bba1a9f519f1b74473ccff47e49eeb7406339817
ekzyis <r.gill@stud.uni-heidelberg.de>
2018-12-26 22:32:05 +0100

    Added example from linuxhowtos.org
    Restructured example folder.
    Added file watcher for:
    - Add new line to end of file
    - Remove trailing new lines
    - Remove trailing whitespaces

--- example/geeksforgeeks.org/server.cpp
+++ example/geeksforgeeks.org/server.cpp
@@ -16,7 +16,7 @@ int main(int argc, char const *argv[])
   int addrlen = sizeof(address);
   char buffer[1024] = {0};
   char const *hello = "Hello from server";
-  
+
   // Creating socket file descriptor
   if ((server_fd  = socket(AF_INET, SOCK_STREAM, 0)) == 0)
   {
